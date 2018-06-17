b95ffbe88c9c4c113cec71d9c6bf73cd80af1e4e
thanhquangute <thanhquang.it.ute@gmail.com>
2018-06-17 22:08:57 +0700

    f

---
+++ Baia1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baia1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Bạn có muốn thoát", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question)==DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text +  " " + textBox2.Text;
+        }
+    }
+}
